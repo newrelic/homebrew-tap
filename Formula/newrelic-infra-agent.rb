67ce4f6e8caf950c7b1d73cc343cd0448f431973
rubenruizdegauna <rubenruizdegauna@gmail.com>
2023-03-07 14:03:58 +0100

    chore: bump infra agent version to 1.38.0 (#36)

--- Formula/newrelic-infra-agent.rb
+++ Formula/newrelic-infra-agent.rb
@@ -2,8 +2,8 @@ class NewrelicInfraAgent < Formula
   desc "New Relic infrastructure agent"
   homepage "https://github.com/newrelic/infrastructure-agent"
   url "https://github.com/newrelic/infrastructure-agent.git",
-      tag:      "1.37.1",
-      revision: "d2755a8da4c943e6833ca0a3be9918c050e1c31a"
+      tag:      "1.38.0",
+      revision: "5f1d8b89949237f24469b5d46f4dbb4f34ea7ac7"
   license "Apache-2.0"
   head "https://github.com/newrelic/infrastructure-agent.git", branch: "master"
 
