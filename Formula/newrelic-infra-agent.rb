29af9f511364f4d92451c1d778346c1d8059b5e7
David Sánchez <davidslt+git@pm.me>
2024-05-06 13:51:49 +0100

    chore: update newrelic-infra-agent 1.52.1

--- Formula/newrelic-infra-agent.rb
+++ Formula/newrelic-infra-agent.rb
@@ -2,8 +2,8 @@ class NewrelicInfraAgent < Formula
   desc "New Relic infrastructure agent"
   homepage "https://github.com/newrelic/infrastructure-agent"
   url "https://github.com/newrelic/infrastructure-agent.git",
-      tag:      "1.50.0",
-      revision: "1be5c0793dfacbc5afee76316dd141623a6f76ac"
+      tag:      "1.52.1",
+      revision: "2f3cfea8406815c1d81d0bea05a83d80cfb279f4"
   license "Apache-2.0"
   head "https://github.com/newrelic/infrastructure-agent.git", branch: "master"
 
@@ -14,7 +14,17 @@ class NewrelicInfraAgent < Formula
     strategy :github_latest
   end
 
-  depends_on "go@1.19" => :build
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "fad1dfda1783ca6322973d106988343679c2679628e2491058a04657aab86c9d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7c8de5bec0fd7949bb5ed0570356ee298fcca257ce058876f4573dc17571056a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "efc6ae9085cadd7bdb86fdb07b431e308bf2d2e51a47a522eaeee7984c0ac5ac"
+    sha256 cellar: :any_skip_relocation, sonoma:         "92ac2d890646270c34d58abec0b9a4e05aae8578397719021f011e86ecf42ea4"
+    sha256 cellar: :any_skip_relocation, ventura:        "b4dfe3a2aa69219f39c4590a6d4c7aebe1aa91c044ca916635f7a3279b42962c"
+    sha256 cellar: :any_skip_relocation, monterey:       "fbdbbac380559ca29e33b446f420c8004b6d0fe91decb9bc24a6e560a3e7790c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9a402c766d73efc89fe88db74a5dc2abe904612f0453df1bfdde9fc46002a677"
+  end
+
+  depends_on "go" => :build
 
   def install
     goarch = Hardware::CPU.intel? ? "amd64" : Hardware::CPU.arch.to_s
