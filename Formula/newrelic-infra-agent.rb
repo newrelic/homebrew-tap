6de281f82ed89ae967f79547279cf5884a0cb69b
sairaj18 <37768344+sairaj18@users.noreply.github.com>
2025-06-27 16:07:41 +0530

    chore: bump agent version 1.65.1 (#83)

--- Formula/newrelic-infra-agent.rb
+++ Formula/newrelic-infra-agent.rb
@@ -2,8 +2,8 @@ class NewrelicInfraAgent < Formula
   desc "New Relic infrastructure agent"
   homepage "https://github.com/newrelic/infrastructure-agent"
   url "https://github.com/newrelic/infrastructure-agent.git",
-      tag:      "1.64.0",
-      revision: "90fee1410132c4887dac5546699c6ebfb7f5aa19"
+      tag:      "1.65.1",
+      revision: "003aeeba6c6c80767dd23260e67fa399353bd779"
   license "Apache-2.0"
   head "https://github.com/newrelic/infrastructure-agent.git", branch: "master"
 
