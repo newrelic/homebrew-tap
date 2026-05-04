cbfc9bb5ef48cbd6b9512a8584a80bcfd9bbf054
Sai Raj <37768344+sairaj18@users.noreply.github.com>
2026-05-04 17:14:21 +0530

    Update newrelic-infra-agent to 1.74.2 (#95)

--- Formula/newrelic-infra-agent.rb
+++ Formula/newrelic-infra-agent.rb
@@ -2,8 +2,8 @@ class NewrelicInfraAgent < Formula
   desc "New Relic infrastructure agent"
   homepage "https://github.com/newrelic/infrastructure-agent"
   url "https://github.com/newrelic/infrastructure-agent.git",
-      tag:      "1.73.1",
-      revision: "b4661314be94ffb3f5cb71ff88515617f3e1b8c2"
+      tag:      "1.74.2",
+      revision: "fc054805a06d651677ef66421f6d2f0137dfa03e"
   license "Apache-2.0"
   head "https://github.com/newrelic/infrastructure-agent.git", branch: "master"
 
